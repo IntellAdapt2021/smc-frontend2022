25c17c95fa8f0d8fd907a9a1cbb8cba1c9b02078
binodpanta <binod80@gmail.com>
2018-10-02 22:50:45 -0400

    Initial NativeScript VueJS app

---
+++ sharedweb/README.md
@@ -0,0 +1,19 @@
+# sharedweb
+
+> p2p shared web ui code
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Build for production
+tns build <platform> --bundle
+
+# Build, watch for changes and debug the application
+tns debug <platform> --bundle
+
+# Build, watch for changes and run the application
+tns run <platform> --bundle
+```
---
+++ sharedweb/app/components/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <Page>
+        <ActionBar title="Power2Peer Blockchain Enabled Trading!" android:flat="true"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                 android:tabTextColor="#c4ffdf"
+                 android:selectedTabTextColor="#ffffff"
+                 androidSelectedTabHighlightColor="#ffffff">
+            <TabViewItem title="Battery">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Community">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Forum">
+                <GridLayout colums="*" rows="*">
+                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Power2Peer'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: rgba(47, 95, 141, 0.467);
+        color: #ffffff;
+    }
+
+    .message { 
+        vertical-align: center;
+        text-align: center;
+        font-size: 30;
+        color: #333333;
+    }
+</style>
