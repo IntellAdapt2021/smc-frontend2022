e4892c3567fcf2f585f60a7a3d854f347de68bc4
binodpanta <binod80@gmail.com>
2018-10-07 23:11:46 -0400

    show address too

--- mobileapp/app/components/Producer.vue
+++ mobileapp/app/components/Producer.vue
@@ -2,8 +2,9 @@
 <FlexboxLayout>
   <!-- Shows the list item label in the default color and style. -->
   <Label flexGrow="1" :text="user.name" width="100" fontSize="20" marginBottom="5" marginTop="5"/>
-  <Label flexGrow="1" width="100" text="$0.97 kWh"/>
-  <Button flexGrow="1" width="90" text="Buy!"/>
+  <Label flexGrow="1" width="40" text="456 some rd., another city, your state, 12345"/>
+  <Label flexGrow="1" width="30" text="$0.97 kWh"/>
+  <Button flexGrow="1" width="10" text="Buy!"/>
 </FlexboxLayout>
 </template>
 
