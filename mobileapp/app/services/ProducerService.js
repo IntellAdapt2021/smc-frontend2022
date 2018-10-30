82d528eb9c7f116c67683fb9ab47a4261ba8d166
binodpanta <binod80@gmail.com>
2018-10-29 22:43:04 -0400

    fetch ordered by pricePerKwh asc

--- mobileapp/app/services/ProducerService.js
+++ mobileapp/app/services/ProducerService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default class ProducerService {
     getProducers() {
-        return axios.get("http://sdn.power2peer.com/transactionapi/api/registeredusers/search/findByType?type=PRODUCER&sort=pricePerKwh,desc");
-        //return axios.get("http://localhost:8080/api/registeredusers/search/findByType?type=PRODUCER&sort=pricePerKwh,desc");
+        return axios.get(
+            "http://sdn.power2peer.com/transactionapi/api/registeredusers/search/findAllByTypeOrderByPricePerKwhAsc?type=PRODUCER");
+        //return axios.get("http://localhost:8080/api/registeredusers/search/findAllByTypeOrderByPricePerKwhAsc?type=PRODUCER");
     }
 }
