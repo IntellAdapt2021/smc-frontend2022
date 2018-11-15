9b2f33da1fdaf46438070995d1536e957979ca40
binodpanta <binod80@gmail.com>
2018-11-14 22:38:17 -0500

    use geospatical query based url to find producers within 100mi of 0,0 for now

--- mobileapp/app/services/ProducerService.js
+++ mobileapp/app/services/ProducerService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class ProducerService {
     getProducers() {
         return axios.get(
-            "http://sdn.power2peer.com/transactionapi/api/registeredusers/search/findAllByTypeOrderByPricePerKwhAsc?type=PRODUCER");
-        //return axios.get("http://localhost:8080/api/registeredusers/search/findAllByTypeOrderByPricePerKwhAsc?type=PRODUCER");
+            "http://sdn.power2peer.com/transactionapi/api/registeredusers/search/findByTypeAndLocationNearOrderByPricePerKwhAsc?type=PRODUCER&location=0,0&distance=100mi");
+        //return axios.get("http://localhost:8080/api/registeredusers/search/findByTypeAndLocationNearOrderByPricePerKwhAsc?type=PRODUCER&location=0,0&distance=100mi");
     }
 }
